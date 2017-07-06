abb0583b813a693a2aa9f0ab9f7ac4e29388fd92
Ziga Patacko Koderman <ziga.patacko@protonmail.com>
2017-07-06 20:15:11 +0200

    init android app with c++ support

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# electronpass-android
-Android client for ElectronPass.
+# Electronpass Android
+ElectronPass Android client written in Kotlin and C++.
+
+## Development environment
+This Android app is currently developed using Android Studio 3.0 or newer. To run it, you will need at least basic Android SDK packages AND:
+- NDK
+- CMake
+- LLDB
+
+These are all available for download via Android SKD Manager.
+
+**Important notes**:
+- on Linux, you might have wrong version of `libncurses` and clang won't find it - run these commands:
+    ````
+    sudo ln -s /usr/lib/libncursesw.so.6.0 /usr/lib/libncurses.so.5
+    sudo ln -s /usr/lib/libncursesw.so.6.0 /usr/lib/libtinfo.so.5
+    ````
+- instant run might cause problems - to disable it go to `Settings > Build, Execution, Deployment > Instant Run`
+
+## License
+Code in this project is licensed under [GNU GPLv3 license](https://github.com/electronpass/electronpass-android/blob/master/LICENSE). Some third party files are subjective to their respective license.
---
+++ app/src/main/java/io/github/electronpass/MainActivity.kt
@@ -0,0 +1,47 @@
+/*
+This file is part of ElectronPass.
+
+ElectronPass is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ElectronPass is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ElectronPass. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package io.github.electronpass
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Example of a call to a native method
+        sample_text.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
